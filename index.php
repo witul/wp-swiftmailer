893980bc81763fdc5afe4046fc4aa37ef70bb0ba
Lukasz Witkowski <witul@linux.pl>
2013-11-13 21:46:22 +0100

    -----BEGIN PGP SIGNED MESSAGE----- Hash: SHA1
    initial
    -----BEGIN PGP SIGNATURE-----
    Version: GnuPG v2.0.22 (GNU/Linux)
    
    iQEcBAEBAgAGBQJSg+UhAAoJEPiGUJMjY3RapgwH/jIM9Jb54Ei25w9iNVFKgX4U
    AlwJTi25wK9KlzxsVH5X6SzZ8a6I7e7n78mLbGbLSFPpHgZPseSG5VAimw/g1hnM
    LJ2uPIFkLxEbiXLBeZlOrWs3uKVZBTUdPJ2mExQkGGmmZpDor6nfmDh2eRDcpvbI
    nXTmwnIr8l6tZ9DeWTmBMYmeXnVqiTTL4AfLTJbJwDdVTwRtcepZFCjheV6GwsSL
    M7lOBv9hEuPTwlJYnmJNKNsHElmkBd8BOhz+1ahsT3Haql1AX8N+Fh/es+zzBss1
    6g4L8TxCR7lFUyRxL4Uf46RMJAnW3B9tPMtezEft2xZhc2T5eabQr6utyQ04ca4=
    =dfsO
    -----END PGP SIGNATURE-----

---
+++ index.php
@@ -0,0 +1,13 @@
+<?
+/*
+Plugin Name: WP SwiftMailer
+Plugin URI: skyhost.pl
+Description: SwiftMailer wrapper for WP
+Version: 5.0.3
+ */
+
+class WP_Swiftmailer{
+    public static function load(){
+        include "swiftmailer/lib//swift_required.php";
+    }
+}
